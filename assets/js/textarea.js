8841e17d58563716117c583103666b5f69be74f0
k-vernooy <kai.a.vernooy@gmail.com>
2019-11-11 20:54:55 -0500

    Move to global scope, catch tab

--- assets/js/textarea.js
+++ assets/js/textarea.js
@@ -1,31 +1,32 @@
 // FOR ALL THE FUNCTIONS REGARDING THE MAIN TEXTAREA
 
+var numbers = document.getElementById("numbers");
+var textarea = document.getElementById("inputText");
+var bg = document.getElementById("textInput");
+var focusTab = document.getElementsByClassName("focusTab")[0];
+var bar = document.getElementById("blackLine")
+
 function brighten() {
-    document.getElementById("textInput").className += " bright";
-    document.getElementsByClassName("focusTab")[0].className += " bright";
+    bg.className += " bright";
+    focusTab.className += " bright";
 }
 
 function darken() {
-    document.getElementById("textInput").className = "";
-    document.getElementsByClassName("focusTab")[0].className = "tab focusTab";
+    bg.className = "";
+    focusTab.className = "tab focusTab";
 }
 
-function getLineNumber(textarea) {
+function getLineNumber() {
     var lines = textarea.value;
-
     var cursLineCount = lines.substr(0, textarea.selectionStart).split("\n").length;
-    var bar = document.getElementById("blackLine")
     var topdist = 19;
 
     cursLineCount -= 1;
     topdist += cursLineCount * 20;
-
     bar.style.top = topdist + "px";
 
     var lineCount = lines.split(/\r\n|\r|\n/).length;
     var existingNumCount = document.getElementById("numbers").childElementCount;
-
-    var numbers = document.getElementById("numbers");
 
     if ( existingNumCount > lineCount ) {
         for ( var i = 0; i < existingNumCount - lineCount; i++ ) {
@@ -36,5 +37,23 @@ function getLineNumber(textarea) {
         for ( var i = existingNumCount; i < lineCount; i++ ) {
             numbers.innerHTML += "<div class='number'>" + ( i + 1 ) + "</div>"
         }
+    }
+}
+
+if ( textarea.addEventListener ) {
+    textarea.addEventListener('keydown',this.keyHandler,false);
+} 
+else if ( textarea.attachEvent ) {
+    textarea.attachEvent('onkeydown',this.keyHandler) // IE
+}
+
+function keyHandler(e) {
+    var TABKEY = 9;
+    if(e.keyCode == TABKEY) {
+        this.value += "    ";
+        if(e.preventDefault) {
+            e.preventDefault();
+        }
+        return false;
     }
 }
