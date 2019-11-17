7208bcc71f34246b3bd7cb8c993496d4484af255
k-vernooy <kai.a.vernooy@gmail.com>
2019-11-17 17:43:46 -0500

    Contenteditable div instead of textarea

--- assets/js/textarea.js
+++ assets/js/textarea.js
@@ -8,40 +8,42 @@ var bar = document.getElementById("blackLine")
 
 function brighten() {
     bg.className += " bright";
-    focusTab.className += " bright";
+    document.getElementsByClassName("focusTab")[0].className += " bright";
 }
 
 function darken() {
     bg.className = "";
-    focusTab.className = "tab focusTab";
+    document.getElementsByClassName("focusTab")[0].className = "tab focusTab";
 }
 
 function getLineNumber() {
-    // console.log("FIREEEEEE")
 
-    var lines = document.getElementById('inputText').value;
-    var cursLineCount = lines.substr(0, document.getElementById('inputText').selectionStart).split("\n").length;
+// NONE OF THIS WILL WORK ANYMORE, 
+// WE SWITCHED TO A BETTER CONTENTEDITABLE DIV
 
-    cursLineCount -= 1;
-    console.log("cursor on line " + (cursLineCount + 1) )
+    // var lines = document.getElementById('inputText').value;
+    // var cursLineCount = lines.substr(0, document.getElementById('inputText').selectionStart).split("\n").length;
+
+    // cursLineCount -= 1;
+    // console.log("cursor on line " + (cursLineCount + 1) )
     
-    var topdist = 19;
-    topdist += cursLineCount * 20;
-    bar.style.top = topdist + "px";
+    // var topdist = 19;
+    // topdist += cursLineCount * 20;
+    // bar.style.top = topdist + "px";
 
-    var lineCount = lines.split(/\r\n|\r|\n/).length;
-    var existingNumCount = document.getElementById("numbers").childElementCount;
+    // var lineCount = lines.split(/\r\n|\r|\n/).length;
+    // var existingNumCount = document.getElementById("numbers").childElementCount;
 
-    if ( existingNumCount >= lineCount ) {
-        for ( var i = 0; i < existingNumCount - lineCount; i++ ) {
-              numbers.removeChild(numbers.lastChild);
-        }
-    }
-    else {
-        for ( var i = existingNumCount; i < lineCount; i++ ) {
-            numbers.innerHTML += "<div class='number'>" + ( i + 1 ) + "</div>"
-        }
-    }
+    // if ( existingNumCount >= lineCount ) {
+    //     for ( var i = 0; i < existingNumCount - lineCount; i++ ) {
+    //           numbers.removeChild(numbers.lastChild);
+    //     }
+    // }
+    // else {
+    //     for ( var i = existingNumCount; i < lineCount; i++ ) {
+    //         numbers.innerHTML += "<div class='number'>" + ( i + 1 ) + "</div>"
+    //     }
+    // }
 }
 
 // if ( textarea.addEventListener ) {
@@ -49,7 +51,9 @@ function getLineNumber() {
 // } 
 
 $(document).on("keyup", function (event) {
+    
     var key = event.keyCode;
+
     $('#inputText').trigger('change');
         if ( (key >= 37 && key <= 40) || key == 13 || key == 8 ) {
         getLineNumber();
