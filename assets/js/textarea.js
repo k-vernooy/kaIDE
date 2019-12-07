9c1e2b12fa709583fce6ff2927b3792e08e9ebef
k-vernooy <kai.a.vernooy@gmail.com>
2019-12-07 18:15:36 -0500

    Correct creation and navigation of lines

--- assets/js/textarea.js
+++ assets/js/textarea.js
@@ -23,6 +23,28 @@ function keyHandler(event) {
     if ( event.key == "Enter" ) {
         addInputEl();
     }
+    else if ( event.key == "ArrowDown") {
+        focusNextLine();
+    }
+    else if ( event.key == "ArrowUp" ) {
+        focusPreviousLine();
+    }
+    else if ( event.key == "ArrowRight" ) {
+        focusRightLine(event);
+    }
+    else if ( event.key == "Backspace" || event.key == "ArrowLeft" ) {
+        focusLeftLine(event);
+    }
+}
+
+function updateIdList() {
+    for ( var i = 0; i < document.getElementsByClassName("inputEl").length; i++  ) {
+        document.getElementsByClassName("inputEl")[i].id = (i + 1) + "-i";
+    }
+
+    for ( var i = 0; i < document.getElementsByClassName("row").length; i++  ) {
+        document.getElementsByClassName("row")[i].id = (i + 1) + "-r";
+    }
 }
 
 function addNumber() {
@@ -34,10 +56,58 @@ function addNumber() {
     document.getElementById("numbers").appendChild(ap);
 }
 
-function focusNextLine() {
-    // console.log(;
+function removeNumber() {
+    var exist = $("#numbers > div").length;
+    console.log
+    var re = document.getElementsByClassName("number")[exist - 1];
+    re.parentNode.removeChild(re);
+}
 
-    document.getElementById((parseInt(document.activeElement.id.split("-")[0]) + 1) + "-i").focus();
+function focusLeftLine(event) {
+
+    // checks to see if we need to focus 
+    // the previous line due to a movement left of the cursor
+    if (document.activeElement.selectionStart == 0) {
+        event.preventDefault();
+        if (document.activeElement.id.split("-")[0] != 1 && event.key == "Backspace") {
+            removeInputEl(document.activeElement.id.split("-")[0])
+        }
+        else {
+            focusPreviousLine();
+        }
+    };
+}
+
+function focusRightLine(event) {
+    if (document.activeElement.selectionStart == document.activeElement.value.length ) {
+        event.preventDefault();
+        focusNextLine();
+        // if (document.activeElement.id.split("-")[0] != 1 && event.key == "Backspace") {
+        //     removeInputEl(document.activeElement.id.split("-")[0])
+        // }
+        // else {
+        //     focusPreviousLine();
+        // }
+    }
+}
+
+function focusPreviousLine() {
+    var line = document.activeElement.id.split("-")[0] - 1;
+    if ( line != 0 ) {
+        document.getElementById(line + "-i").focus();
+    }
+}
+
+function focusNextLine() {
+    if ( document.activeElement.id.split("-")[0] != document.getElementById("inputArray").childElementCount) {
+        document.getElementById((parseInt(document.activeElement.id.split("-")[0]) + 1) + "-i").focus();
+    };
+}
+
+function moveRenderDiv() {
+    var size = document.getElementsByClassName("inputEl").length;
+    var num = -18 * (size);
+    document.getElementById("render").style.top = num + "px";
 }
 
 function addInputEl() {
@@ -61,22 +131,29 @@ function addInputEl() {
     document.getElementById("inputArray").appendChild(input);
     document.getElementById("render").appendChild(div);
 
-    var num = -18 * (size + 1);
-    document.getElementById("render").style.top = num + "px";
-
+    moveRenderDiv();
     focusNextLine();
-    focusInput();
-
+    updateDarkBar();
     addNumber();
 
-    //remove placeholder
     document.getElementById("1-i").placeholder = ""
 }
 
-function removeInputEl() {
+function removeInputEl(num) {
+    var input = document.getElementById(num + "-i");
+    var raw = document.getElementById(num + "-r");
+    input.parentNode.removeChild(input);
+    raw.parentNode.removeChild(raw);
+
+    updateIdList();
+    moveRenderDiv();
+    removeNumber();
+    if ( num != 1 ) {
+        document.getElementById((num - 1) + "-i" ).focus();
+    }
 }
 
-function focusInput() {
+function updateDarkBar() {
     var id = document.activeElement.id;
     $('#render .dark').removeClass("dark");
     document.getElementById(id.split('-')[0] + "-r").classList.add("dark");
@@ -93,11 +170,13 @@ $( "#inputArray" ).on("keydown", ".inputEl", function(event){
 });
 
 $( "#inputArray" ).on("focus", ".inputEl", function(event){
-    focusInput();
+    updateDarkBar();
 });
 
 $( "#inputArray" ).on("input", "input", function(event){
     renderCont();
 });
 
-focusInput();
+$(document).ready(function() {
+    updateDarkBar();
+});
