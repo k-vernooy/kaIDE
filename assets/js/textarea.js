9f13149436af7f04731650fee9247b2884047472
k-vernooy <kai.a.vernooy@gmail.com>
2019-12-07 11:45:11 -0500

    Added js functions for focusing and creating input children

--- assets/js/textarea.js
+++ assets/js/textarea.js
@@ -50,13 +50,72 @@ function getLineNumber() {
 //     textarea.addEventListener('keydown', this.keyHandler, true );
 // } 
 
-$(document).on("keyup", function (event) {
-    
-    var key = event.keyCode;
-
-    $('#inputText').trigger('change');
-        if ( (key >= 37 && key <= 40) || key == 13 || key == 8 ) {
-        getLineNumber();
+function keyHandler(event) {
+    // console.log("triggered")
+    if ( event.key == "ArrowLeft" || event.key == "ArrowRight" || event.key == "ArrowDown" ||  event.key == "ArrowUp" || event.key == "Enter") {
+        switchLine(event.key);
     }
 
- });
+}
+
+function addNumber() {
+    var exist = $("#numbers > div").length;
+    var ap = document.createElement("div");
+    ap.className = "number";
+    ap.innerHTML = exist + 1;
+
+    document.getElementById("numbers").appendChild(ap);
+}
+
+function addInputEl() {
+    var div = document.createElement('div');
+    var input = document.createElement('input');
+    var size = document.getElementsByClassName("inputEl").length;
+
+    input.className = "inputEl";
+    input.id = (size + 1) + "-i";
+    input.type = "text";
+    input.keydown = function(event){keyHandler(event)}; 
+    div.id = (size + 1 ) + "-r";
+    div.className = "row";
+
+
+    document.getElementById("inputArray").appendChild(input);
+    document.getElementById("render").appendChild(div);
+
+    var num = -18 * (size + 1);
+    document.getElementById("render").style.top = num + "px";
+
+    document.getElementById("inputArray").childNodes[document.getElementById("inputArray").childNodes.length - 1].focus();
+    focusInput();
+
+    addNumber();
+}
+
+function removeInputEl() {
+}
+
+
+function switchLine(key) {
+    var size = document.getElementsByClassName("inputEl").length;
+    var pos = document.activeElement.id.split("-")[0];
+    if ( key == "Enter" && size == pos ) {
+        addInputEl();
+    }
+    console.log("size: " + size);
+    console.log("pos: " + pos);
+}
+
+function focusInput() {
+    var id = document.activeElement.id;
+    $('#render .dark').removeClass("dark");
+    document.getElementById(id.split('-')[0] + "-r").classList.add("dark");
+}
+
+$( "#inputArray" ).on("keydown", ".inputEl", function(event){
+    keyHandler(event);
+});
+
+$( "#inputArray" ).on("focus", ".inputEl", function(event){
+    focusInput();
+});
