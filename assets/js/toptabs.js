8356955e4739b8e9153f49332f13ad2c316c151e
k-vernooy <kai.a.vernooy@gmail.com>
2020-01-11 15:58:17 -0500

    Added multi-file functionality

--- assets/js/toptabs.js
+++ assets/js/toptabs.js
@@ -1,15 +1,35 @@
-function switchTab(tab) {
-    document.getElementsByClassName("focusTab")[0].className = "tab";
-    tab.classList.add("focusTab");
-    document.getElementById("1-i").focus();
+function switchTab(e) {
+    var el = e.target;
+    var id = el.id.split("_")[0];
+
+    var win = document.getElementById(id);
+    var tabs = document.getElementsByClassName("tab");
+    var windows = document.getElementsByClassName("codearea");
+    
+    for ( var i = 0; i < tabs.length; i++) {
+        tabs[i].classList.remove("focusTab");
+    }
+
+    e.target.classList.add("focusTab");
+
+    for ( var i = 0; i < windows.length; i++) {
+        windows[i].style.display = "none";
+    }
+
+    win.style.display = "block";
+    // document.getElementById("1-i").focus();
 }
 
-function brighten() {
-    bg.className += " bright";
-    document.getElementsByClassName("focusTab")[0].className += " bright";
-}
+// function brighten() {
+//     bg.className += " bright";
+//     document.getElementsByClassName("focusTab")[0].className += " bright";
+// }
 
-function darken() {
-    bg.className = "";
-    document.getElementsByClassName("focusTab")[0].className = "tab focusTab";
-}
+// function darken() {
+//     bg.className = "";
+//     document.getElementsByClassName("focusTab")[0].className = "tab focusTab";
+// }
+
+$( ".tab" ).on("click", function(event){
+    switchTab(event);
+});
